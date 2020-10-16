4c5e6ecb81c7d8770fa7c57b6519160224b6c246
P.T.Liu <meow.cs07@nctu.edu.tw>
2020-10-17 00:51:58 +0800

    two line text

--- demo.cpp
+++ demo.cpp
@@ -16,6 +16,8 @@ int main(int argc, char *argv[]) {
         frame_no *= 5;
         for (int i = frame_no; i < frame_no + 5; ++i) {
             cv::Mat frame = cv::imread("../resource/media/" + std::to_string(i) + ".png");
+            if (frame.empty())
+                break;
             std::vector<bbox_t> prediction = detector.detect(frame);
             std::sort(prediction.begin(), prediction.end(), [](const bbox_t &lhs, const bbox_t &rhs) {
                 return lhs.w + lhs.x > rhs.w + rhs.x;
@@ -25,7 +27,8 @@ int main(int argc, char *argv[]) {
                  prediction,
                  {"RSSI: " + std::to_string(rssi[0]) + " MCS: " + std::to_string(mcs[0]),
                   "RSSI: " + std::to_string(rssi[1]) + " MCS: " + std::to_string(mcs[1]),
-                  "RSSI: " + std::to_string(rssi[2]) + " MCS: " + std::to_string(mcs[2])});
+                  "RSSI: " + std::to_string(rssi[2]) + " MCS: " + std::to_string(mcs[2])},
+                 {"MAC: 38:78:62:46:3d:cc", "MAC: b0:6e:bf:04:bc:4c", "MAC: 04:92:26:7e:18:00"});
 
             cv::imwrite("../out/" + std::to_string(i) + ".png", frame, {cv::IMWRITE_PNG_COMPRESSION, 9});
             cv::imshow("Demo", frame);
--- marker.hpp
+++ marker.hpp
@@ -8,17 +8,29 @@
 
 #include "darknet.hpp"
 
-void mark(cv::Mat image, std::vector<bbox_t> bounding_boxes, const std::vector<std::string> &MAC) {
+void mark(cv::Mat image,
+          std::vector<bbox_t> bounding_boxes,
+          const std::vector<std::string> &info,
+          const std::vector<std::string> &MAC) {
     const cv::Scalar box_color = CV_RGB(255, 0, 255);
     const cv::Scalar text_color = CV_RGB(255, 0, 0);
+
+    constexpr double scale = 1.0;
+    constexpr int thick = 3;
 
     std::size_t shortest = std::min(bounding_boxes.size(), MAC.size());
     for (std::size_t i = 0; i < shortest; ++i) {
         const bbox_t &box = bounding_boxes[i];
-        int baseline;
-        cv::Size text_box = cv::getTextSize(MAC[i], cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseline);
-        cv::Point text_position(box.x - (text_box.width - box.w) / 2, box.y - baseline);
-        cv::putText(image, MAC[i], text_position, cv::FONT_HERSHEY_SIMPLEX, 1.1, text_color, 3, cv::LINE_AA);
+        int mac_baseline, text_baseline;
+        cv::Size mac_box = cv::getTextSize(MAC[i], cv::FONT_HERSHEY_SIMPLEX, scale, thick, &mac_baseline);
+        cv::Point mac_position(box.x - (mac_box.width - box.w) / 2, box.y - mac_baseline);
+        cv::putText(image, MAC[i], mac_position, cv::FONT_HERSHEY_SIMPLEX, scale, text_color, thick, cv::LINE_AA);
+
+        cv::Size text_box = cv::getTextSize(info[i], cv::FONT_HERSHEY_SIMPLEX, scale, thick, &text_baseline);
+        cv::Point text_position(box.x - (text_box.width - box.w) / 2,
+                                box.y - text_baseline - mac_baseline - mac_box.height);
+        cv::putText(image, info[i], text_position, cv::FONT_HERSHEY_SIMPLEX, scale, text_color, thick, cv::LINE_AA);
+
         cv::rectangle(image, cv::Rect(box.x, box.y, box.w, box.h), box_color, 2, cv::LINE_8);
     }
 }
